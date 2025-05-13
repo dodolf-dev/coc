ed87edb56b86b24ae29e274007ba033d5ee4209e
dodolf-dev <grimelins1234@gmail.com>
2025-05-13 11:28:38 +0200

    dd

--- code/village principal/batiments/web display/defense display/tour bombe display/tbombe2 display.js
+++ code/village principal/batiments/web display/defense display/tour bombe display/tbombe2 display.js
@@ -30,7 +30,7 @@ function updateCanonOptions() {
         const level = parseInt(key.split("_").pop());
         const option = document.createElement("option");
         option.value = level;
-        option.textContent = `Tour bombe 1 Niveau ${level}`;
+        option.textContent = `Tour bombe 2 Niveau ${level}`;
         selecttour_bombe2.appendChild(option);
 
         // Si l'ancien niveau est toujours disponible, on le sélectionne
