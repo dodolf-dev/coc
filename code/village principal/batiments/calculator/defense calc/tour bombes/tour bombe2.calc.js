60d412d9cac2af00b05d99dbeef22f2576248ad9
dodolf-dev <grimelins1234@gmail.com>
2025-05-13 11:18:36 +0200

    ajout des tour a bombes

---
+++ code/village principal/batiments/calculator/defense calc/tour bombes/tour bombe2.calc.js
@@ -0,0 +1,173 @@
+import { tour_bombe2 } from '/coc/code/village principal/batiments/database/data defense/data tour bombe.js';
+
+//général
+export function tour_bombe2_nv_max_hdv(hdvNiveau) {
+    let niveauMax = 0;
+    const niveaux = Object.keys(tour_bombe2)
+        .map(key => parseInt(key.replace("tour_bombe2_nv_", ""), 10))
+        .filter(n => !isNaN(n)); 
+
+    const niveauMaxPossible = Math.max(...niveaux);
+    for (let i = 0; i <= niveauMaxPossible; i++) {
+        if (tour_bombe2[`tour_bombe2_nv_${i}`].hdvrequis <= hdvNiveau) {
+            niveauMax = i;
+        }
+}
+    return niveauMax;
+}
+
+//calcul de temps
+
+//temps passer a construire le canon
+export function calculerTempsTotaltour_bombe2(niveauMax) {
+    let tempsTotal = 0;
+    for (let i = 1; i <= niveauMax; i++) {
+        tempsTotal += tour_bombe2[`tour_bombe2_nv_${i}`].tconstru;
+    }
+    return tempsTotal;
+}
+
+//calcul le temps de construction restant pour le canon
+export function calculerTempsRestanttour_bombe2(niveauActuel, niveauMax) {
+    let tempsRestant = 0;
+    for (let i = niveauActuel + 1; i <= niveauMax; i++) {
+        const key = `tour_bombe2_nv_${parseInt(i, 10)}`; // Supprime les zéros inutiles
+        if (tour_bombe2.hasOwnProperty(key)) {
+            tempsRestant += tour_bombe2[key].tconstru;
+        } else {
+            console.warn(`La clé ${key} est introuvable dans l'objet tour_bombe2.`);
+        }
+    }
+    return tempsRestant;
+}
+
+//calcul le temps de construction par rapport a l'hdv hdv pour le canon
+export function calculerTempsdepuisHDVtour_bombe2(hdvNiveau) {
+    let tempsTotal = 0;
+    const niveauMax = Math.max(...Object.keys(tour_bombe2)
+        .map(key => parseInt(key.replace("tour_bombe2_nv_", ""), 10))
+        .filter(n => !isNaN(n))
+    );
+    for (let i = 0; i <= niveauMax; i++) {
+        const canon = tour_bombe2[`tour_bombe2_nv_${i}`];
+        if (canon.hdvrequis <= hdvNiveau) {
+            tempsTotal += canon.tconstru;
+        }
+    }
+    return tempsTotal;
+}
+
+//calcul le temps de construction pour l'hdv séléctionner pour le canon
+export function calculerTempsConstructionParHDVtour_bombe2(hdvNiveau) {
+    let tempsTotal = 0;
+
+    for (let i = 1; i <= 21; i++) {
+        const canon = tour_bombe2[`tour_bombe2_nv_${i}`];
+        
+        if (canon.hdvrequis === hdvNiveau) {
+            tempsTotal += canon.tconstru;
+        }
+    }
+    return tempsTotal;
+}
+
+//calcul de prix
+
+//prix déjà payer pour le canon
+export function calculerPrixTotaltour_bombe2(niveauMax) {
+    let prixTotal = 0;
+    for (let i = 1; i <= niveauMax; i++) {
+        prixTotal += tour_bombe2[`tour_bombe2_nv_${i}`].prix;
+    }
+    return prixTotal;
+}
+
+//calcul le prix restant pour le canon
+export function calculerPrixRestanttour_bombe2(niveauActuel, niveauMax) {
+    let prixRestant = 0;
+    for (let i = niveauActuel + 1; i <= niveauMax; i++) {
+        prixRestant += tour_bombe2[`tour_bombe2_nv_${i}`].prix;
+    }
+    return prixRestant;
+}
+
+//calcul le prix de construction par rapport a l'hdv pour le canon
+export function calculerPrixdepuisHDVtour_bombe2(hdvNiveau) {
+    let prixTotal = 0;
+    const niveauMax = Math.max(...Object.keys(tour_bombe2)
+        .map(key => parseInt(key.replace("tour_bombe2_nv_", ""), 10))
+        .filter(n => !isNaN(n))
+    );
+    for (let i = 0; i <= niveauMax; i++) {
+        const canon = tour_bombe2[`tour_bombe2_nv_${i}`];
+        if (canon.hdvrequis <= hdvNiveau) {
+            prixTotal += canon.prix;
+        }
+    }
+    return prixTotal;
+}
+
+//calcul le prix de construction pour l'hdv séléctionner pour le canon
+export function calculerPrixConstructionParHDVtour_bombe2(hdvNiveau) {
+    let prixTotal = 0;
+
+    for (let i = 1; i <= 21; i++) {
+        const canon = tour_bombe2[`tour_bombe2_nv_${i}`];
+        
+        if (canon.hdvrequis === hdvNiveau) {
+            prixTotal += canon.prix;
+        }
+    }
+    return prixTotal;
+}
+
+
+//calcul d'expérience
+
+//éxpérience déjà gagner pour le canon
+export function calculerExperienceTotaltour_bombe2(niveauMax) {
+    let experienceTotal = 0;
+    for (let i = 1; i <= niveauMax; i++) {
+        experienceTotal += tour_bombe2[`tour_bombe2_nv_${i}`].experience;
+    }
+    return experienceTotal;
+}
+
+//calcul l'éxpérience restant pour le canon
+export function calculerExperienceRestanttour_bombe2(niveauActuel, niveauMax) {
+    let experienceRestant = 0;
+    for (let i = niveauActuel + 1; i <= niveauMax; i++) {
+        experienceRestant += tour_bombe2[`tour_bombe2_nv_${i}`].experience;
+    }
+    return experienceRestant;
+}
+
+//calcul l'éxpérience de construction par rapport a l'hdv pour le canon
+export function calculerExperiencedepuisHDVtour_bombe2(hdvNiveau) {
+    let experienceTotal = 0;
+    const niveauMax = Math.max(...Object.keys(tour_bombe2)
+        .map(key => parseInt(key.replace("tour_bombe2_nv_", ""), 10))
+        .filter(n => !isNaN(n))
+    );
+    for (let i = 0; i <= niveauMax; i++) {
+        const canon = tour_bombe2[`tour_bombe2_nv_${i}`];
+        if (canon.hdvrequis <= hdvNiveau) {
+            experienceTotal += canon.experience;
+        }
+    }
+    return experienceTotal;
+}
+
+//calcul l'éxpérience de construction pour l'hdv séléctionner pour le canon
+export function calculerExperienceConstructionParHDVtour_bombe2(hdvNiveau) {
+    let experienceTotal = 0;
+
+    for (let i = 1; i <= 21; i++) {
+        const canon = tour_bombe2[`tour_bombe2_nv_${i}`];
+        
+        if (canon.hdvrequis === hdvNiveau) {
+            experienceTotal += canon.experience;
+        }
+    }
+    return experienceTotal;
+}
