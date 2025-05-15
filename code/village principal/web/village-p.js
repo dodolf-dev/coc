f93d899dac6d8a6134576546cecbaf7ba53c4c87
dodolf-dev <grimelins1234@gmail.com>
2025-05-15 22:44:44 +0200

    V0.0.2

--- code/village principal/web/village-p.js
+++ code/village principal/web/village-p.js
@@ -15,4 +15,51 @@ document.addEventListener('DOMContentLoaded', () => {
   
       observer.observe(img, { attributes: true });
     });
-  });
+
+    // Ajoute les écouteurs pour HDV, canons et tours d'archères
+const hdvSelect = document.getElementById('hdv');
+    if (hdvSelect) {
+        hdvSelect.addEventListener('change', updateDefensePrixOr);
+    }
+
+    // Canons
+    for (let i = 1; i <= 7; i++) {
+        const canonSelect = document.getElementById(`canon${i}`);
+        if (canonSelect) {
+            canonSelect.addEventListener('change', updateDefensePrixOr);
+        }
+    }
+
+    // Tours d'archères
+    for (let i = 1; i <= 9; i++) {
+        const tourArchereSelect = document.getElementById(`tour_archere${i}`);
+        if (tourArchereSelect) {
+            tourArchereSelect.addEventListener('change', updateDefensePrixOr);
+        }
+    }
+
+    // Appel initial
+    updateDefensePrixOr();
+});
+
+import { globalcalculerPrixRestantcanon } from "/coc/code/village principal/batiments/calculator/defense calc/canons/canon_global_calc.js";
+import { globalcalculerPrixRestanttour_archere } from "/coc/code/village principal/batiments/calculator/defense calc/tour archeres/tour archere global calc.js";
+import { formatPrix } from "/coc/code/outils/affichge nombre.js";
+
+function updateDefensePrixOr() {
+    const progressDefensePrixOr = document.getElementById('progress-defense-prix-or');
+    if (!progressDefensePrixOr) return;
+
+    // Additionne le coût restant des canons et des tours d'archères
+    const prixRestantCanons = globalcalculerPrixRestantcanon();
+    const prixRestantTourArchere = globalcalculerPrixRestanttour_archere();
+    const totalPrixRestant = prixRestantCanons + prixRestantTourArchere;
+
+    if (totalPrixRestant === 0) {
+        progressDefensePrixOr.style.display = "none";
+    } else {
+        progressDefensePrixOr.style.display = "";
+        progressDefensePrixOr.innerHTML = `${formatPrix(totalPrixRestant)} <img src="/coc/image/village principal/ressource/or village-p.jpg" alt="or" class="icone-ressource">`;
+    }
+}
+updateDefensePrixOr();
