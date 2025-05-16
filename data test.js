c63affa7eddfb212a438e82b7f1e584eb33d72f5
dodolf-dev <grimelins1234@gmail.com>
2025-05-16 10:50:35 +0200

    Update data test.js

--- data test.js
+++ data test.js
@@ -66,3 +66,13 @@ export const usine_sorts = {
     usine_sorts_nv_7: { pv: 960, capacité: 10, prix: 9000000, tconstru: 604800, exp_gagner: 777, hdvrequis: 13, image: "/coc/image/village principal/batiment/armee/usine sorts/usinesorts7.png" },
     usine_sorts_nv_8: { pv: 1080, capacité: 10, prix: 14000000, tconstru: 691200, exp_gagner: 831, hdvrequis: 15, image: "/coc/image/village principal/batiment/armee/usine sorts/usinesorts8.png" },
 };
+
+export const usine_sorts_noir = {
+    usine_sorts_noir_nv_0: { pv: 0, capacité: 0, prix: 0, tconstru: 0, exp_gagner: 0, hdvrequis: 8, image: "/coc/image/village principal/batiment/armee/usine sorts noir/usinesortsnoir0.png" },
+    usine_sorts_noir_nv_1: { pv: 600, capacité: 1, prix: 130000, tconstru: 21600, exp_gagner: 146, hdvrequis: 8, image: "/coc/image/village principal/batiment/armee/usine sorts noir/usinesortsnoir1.png" },
+    usine_sorts_noir_nv_2: { pv: 660, capacité: 1, prix: 260000, tconstru: 43200, exp_gagner: 207, hdvrequis: 8, image: "/coc/image/village principal/batiment/armee/usine sorts noir/usinesortsnoir2.png" },
+    usine_sorts_noir_nv_3: { pv: 720, capacité: 1, prix: 600000, tconstru: 172800, exp_gagner: 415, hdvrequis: 9, image: "/coc/image/village principal/batiment/armee/usine sorts noir/usinesortsnoir3.png" },
+    usine_sorts_noir_nv_4: { pv: 780, capacité: 1, prix: 1200000, tconstru: 345600, exp_gagner: 587, hdvrequis: 9, image: "/coc/image/village principal/batiment/armee/usine sorts noir/usinesortsnoir4.png" },
+    usine_sorts_noir_nv_5: { pv: 840, capacité: 1, prix: 2500000, tconstru: 432000, exp_gagner: 657, hdvrequis: 10, image: "/coc/image/village principal/batiment/armee/usine sorts noir/usinesortsnoir5.png" },
+    usine_sorts_noir_nv_6: { pv: 950, capacité: 1, prix: 4000000, tconstru: 518400, exp_gagner: 720, hdvrequis: 12, image: "/coc/image/village principal/batiment/armee/usine sorts noir/usinesortsnoir6.png" },
+};
