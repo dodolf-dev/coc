4230444be4090c143c08bf72c6e8c019e94f1a5c
dodolf-dev <grimelins1234@gmail.com>
2025-05-16 12:38:50 +0200

    Update data test.js

--- data test.js
+++ data test.js
@@ -76,3 +76,18 @@ export const usine_sorts_noir = {
     usine_sorts_noir_nv_5: { pv: 840, capacité: 1, prix: 2500000, tconstru: 432000, exp_gagner: 657, hdvrequis: 10, image: "/coc/image/village principal/batiment/armee/usine sorts noir/usinesortsnoir5.png" },
     usine_sorts_noir_nv_6: { pv: 950, capacité: 1, prix: 4000000, tconstru: 518400, exp_gagner: 720, hdvrequis: 12, image: "/coc/image/village principal/batiment/armee/usine sorts noir/usinesortsnoir6.png" },
 };
+
+export const hall_heros = {
+    hall_heros_nv_0: { pv: 0, prix: 0, tconstru: 0, exp_gagner: 0, hdvrequis: 3, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros0.png" },
+    hall_heros_nv_1: { pv: 2000, prix: 800000, tconstru: 86400, exp_gagner: 293, hdvrequis: 7, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros1.png" },
+    hall_heros_nv_2: { pv: 2400, prix: 1600000, tconstru: 172800, exp_gagner: 415, hdvrequis: 8, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros2.png" },
+    hall_heros_nv_3: { pv: 2800, prix: 2300000, tconstru: 259200, exp_gagner: 509, hdvrequis: 9, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros3.png" },
+    hall_heros_nv_4: { pv: 3200, prix: 3000000, tconstru: 432000, exp_gagner: 657, hdvrequis: 10, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros4.png" },
+    hall_heros_nv_5: { pv: 3600, prix: 5000000, tconstru: 518400, exp_gagner: 720, hdvrequis: 11, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros5.png" },
+    hall_heros_nv_6: { pv: 3800, prix: 6000000, tconstru: 604800, exp_gagner: 777, hdvrequis: 12, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros6.png" },
+    hall_heros_nv_7: { pv: 4200, prix: 9000000, tconstru: 648000, exp_gagner: 804, hdvrequis: 13, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros7.png" },
+    hall_heros_nv_8: { pv: 4600, prix: 10000000, tconstru: 676800, exp_gagner: 818, hdvrequis: 14, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros8.png" },
+    hall_heros_nv_9: { pv: 5000, prix: 12000000, tconstru: 691200, exp_gagner: 831, hdvrequis: 15, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros9.png" },
+    hall_heros_nv_10: { pv: 5400, prix: 14000000, tconstru: 777600, exp_gagner: 881, hdvrequis: 16, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros10.png" },
+    hall_heros_nv_11: { pv: 5800, prix: 21000000, tconstru: 1339200, exp_gagner: 1157, hdvrequis: 17, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros11.png" },
+};
