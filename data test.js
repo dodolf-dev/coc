a8d16c60db018305256cf6fbb527227ead37a973
dodolf-dev <grimelins1234@gmail.com>
2025-05-16 12:55:30 +0200

    Update data test.js

--- data test.js
+++ data test.js
@@ -91,3 +91,15 @@ export const hall_heros = {
     hall_heros_nv_10: { pv: 5400, prix: 14000000, tconstru: 777600, exp_gagner: 881, hdvrequis: 16, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros10.png" },
     hall_heros_nv_11: { pv: 5800, prix: 21000000, tconstru: 1339200, exp_gagner: 1157, hdvrequis: 17, image: "/coc/image/village principal/batiment/armee/hall des heros/halldesheros11.png" },
 };
+
+export const atelier = {
+    atelier_nv_0: { pv: 0, capacité: 0, prix: 0, tconstru: 0, exp_gagner: 0, hdvrequis: 12, image: "/coc/image/village principal/batiment/armee/atelier/atelier0.png" },
+    atelier_nv_1: { pv: 1000, capacité: 1, prix: 2400000, tconstru: 172800, exp_gagner: 415, hdvrequis: 12, image: "/coc/image/village principal/batiment/armee/atelier/atelier1.png" },
+    atelier_nv_2: { pv: 1100, capacité: 2, prix: 3700000, tconstru: 259200, exp_gagner: 509, hdvrequis: 12, image: "/coc/image/village principal/batiment/armee/atelier/atelier2.png" },
+    atelier_nv_3: { pv: 1200, capacité: 3, prix: 5000000, tconstru: 345600, exp_gagner: 587, hdvrequis: 12, image: "/coc/image/village principal/batiment/armee/atelier/atelier3.png" },
+    atelier_nv_4: { pv: 1300, capacité: 3, prix: 8700000, tconstru: 604800, exp_gagner: 777, hdvrequis: 13, image: "/coc/image/village principal/batiment/armee/atelier/atelier4.png" },
+    atelier_nv_5: { pv: 1400, capacité: 3, prix: 9000000, tconstru: 626400, exp_gagner: 791, hdvrequis: 13, image: "/coc/image/village principal/batiment/armee/atelier/atelier5.png" },
+    atelier_nv_6: { pv: 1500, capacité: 3, prix: 10000000, tconstru: 648000, exp_gagner: 804, hdvrequis: 14, image: "/coc/image/village principal/batiment/armee/atelier/atelier6.png" },
+    atelier_nv_7: { pv: 1600, capacité: 3, prix: 11000000, tconstru: 676800, exp_gagner: 818, hdvrequis: 15, image: "/coc/image/village principal/batiment/armee/atelier/atelier7.png" },
+    atelier_nv_8: { pv: 1700, capacité: 3, prix: 13000000, tconstru: 691200, exp_gagner: 831, hdvrequis: 16, image: "/coc/image/village principal/batiment/armee/atelier/atelier8.png" },
+};
